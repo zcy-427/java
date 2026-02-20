8fc78cabc913c91a603b3950f059e3cbe52df39e
Amadeus <2593251697@qq.com>
2026-02-20 14:08:55 +0800

    vault backup: 2026-02-20 14:08:55

--- javaweb/js/代码/BOM/test.js
+++ javaweb/js/代码/BOM/test.js
@@ -1,7 +1,19 @@
-let time = document.getElementById("body");
-time = setInterval(() => {
-    console.log("减少1秒");
-   setTimeout(() => {
-    location.href = "https://www.baidu.com";      
-   }, 5000);
-}, 1000);
+const timeSpan = document.getElementById('time'); 
+const button = document.getElementById('return');
+let timeLeft=5; // 倒计时的秒数
+const targetUrl = 'https://www.baidu.com'; // 目标 URL
+let timer = setInterval(function() {
+        timeLeft--; // 时间减 1
+        timeSpan.innerText = timeLeft; // 更新页面上的数字 (DOM 核心)
+
+        // 4. 判断是否到 0 了
+        if (timeLeft <= 0) {
+            clearInterval(timer); //拔掉定时器的电源！(非常重要，防止内存泄漏)
+            location.href = targetUrl; // 执行跳转 (BOM 核心)
+        }
+    }, 1000);
+
+button.addEventListener('click', function() {
+    clearInterval(timer); //拔掉定时器的电源！(非常重要，防止内存泄漏 )
+    location.href = targetUrl; // 执行跳转 (BOM 核心)
+});
